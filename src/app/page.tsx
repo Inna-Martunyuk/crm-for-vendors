f971db1b5fd229c11803224dd56485ea84e819cf
Inna-Martunyuk <imartunyuk88@gmail.com>
2025-09-05 17:31:56 +0300

    crm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import AddCompanyButton from '@/app/components/add-company-button';
+import MagicButton from './components/magic-button';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
+      <AddCompanyButton />
+      <MagicButton/>
     </main>
   );
 }
